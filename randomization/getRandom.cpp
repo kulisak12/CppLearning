acfdb7d5cdf53c78e1bbd0e26ce9eba27da42a12
kulisak12 <kulisak12lp@gmail.com>
2018-02-25 20:54:16 +0100

    Moved to Visual Studio
    Created an individual Visual Studio project for each test file
    Changed .gitignore to ignore VS files

--- randomization/getRandom.cpp
+++
@@ -1,5 +0,0 @@
-// return a random number from <0;1)
-float getRandom() {
-    float rnd = rand(); // the seed from main() still has an effect
-    return rnd / (RAND_MAX + 1); // +1 creates <0;1), otherwise it is <0;1>
-}
