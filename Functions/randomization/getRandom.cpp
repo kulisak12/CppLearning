429f341b3a9f545472f0506b15ed5a8374dd47dc
kulisak12 <kulisak12lp@gmail.com>
2018-03-20 22:43:07 +0100

    Changed code to keep the console open
    Changed cin buffer operations to use cin.ignore
    Moved solution and project files to the same folder
    Renamed projects to camelCase
    Removed unnecessary project-specific ignores from .gitignore

---
+++ Functions/randomization/getRandom.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+#include <cstdlib>
+
+// return a random number from <0;1)
+float getRandom() {
+	float rnd = std::rand(); // the seed from main() still has an effect
+	return rnd / (RAND_MAX + 1); // +1 creates <0;1), otherwise it is <0;1>
+}
