ee6c635672fb2e9f8352c3fb7bd2519b795e74dc
kulisak12 <kulisak12lp@gmail.com>
2018-05-01 09:10:23 +0200

    New project constVariables

---
+++ DataTypes/constVariables/constants.cpp
@@ -0,0 +1,10 @@
+#include "stdafx.h"
+
+// create constant variables
+namespace constants {
+	extern const double pi = 3.141592653;
+	extern const double gravity = 9.81;
+}
+
+extern const int itemsInStack = 64;
+extern const int inventorySlotCount = 36;
---
+++ DataTypes/constVariables/constants.h
@@ -0,0 +1,10 @@
+#pragma once
+
+// forward declare constant variables
+namespace constants {
+	extern const double pi;
+	extern const double gravity;
+}
+
+extern const int itemsInStack;
+extern const int inventorySlotCount;
